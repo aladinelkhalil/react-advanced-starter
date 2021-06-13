8bca6213a1d4a4dc897da8faaf3749bd0343bc87
Marc Klefter <marc.klefter@edument.se>
2021-06-13 22:38:37 +0200

    Fixed import

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ import reportWebVitals from './reportWebVitals';
 
 // ...
 // Uncomment an example to run it.
-// import { App } from './useReducer';
+import { App } from './useReducer';
 // import { App } from './useStateImpl';
 // import { App } from './useTask';
 // import { App } from './dependencyContext';
 // import { App } from './errorBoundary';
 // import { App } from './authContext';
-import { App } from './todo';
+// import { App } from './todo';
 
 // ...
 
